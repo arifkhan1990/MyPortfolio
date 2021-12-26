cf588569383c77e25a26aa89ea363f2cf2e0a813
arifkhan1990 <arifkhanshubro@gmail.com>
2021-12-27 02:16:18 +0600

    design leading page

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import Typical from 'react-typical';
-
+import "./Profile.css";
 export default function Profile() {
     return (
         <div className='profile-container'>
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='cloz'>
-                        <a href='https://www.facebook.com/arif.khan.shubro/'>
-                            <i className='fa fa-facebook-square'></i>
-                        </a>
-                        <a href='https://www.instagram.com/arif.khan.1990/'>
-                            <i className='fa fa-instagram'></i>
-                        </a>
-                        <a href='https://www.linkedin.com/in/arif-khan-2b4b23118/'>
-                            <i className='fa fa-linkedin-square'></i>
-                        </a>
-                        <a href='#'>
-                            <i className='fa fa-google-plus-square'></i>
-                        </a>
-                        <a href='#'>
-                            <i className='fa fa-twitter-square'></i>
-                        </a>
+                        <div className="cloz-icon">
+                            <a href='https://www.facebook.com/arif.khan.shubro/'>
+                                <i className='fa fa-facebook-square'></i>
+                            </a>
+                            <a href='https://www.instagram.com/arif.khan.1990/'>
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                            <a href='https://www.linkedin.com/in/arif-khan-2b4b23118/'>
+                                <i className='fa fa-linkedin-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-google-plus-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-twitter-square'></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
 
@@ -52,10 +54,24 @@ export default function Profile() {
                             ]}
                             />
                         </h1>
+                        <span className="profoile-role-tagline">
+                            Hire me Or Knock of building application with front and back end operation.
+                        </span>
                     </span>
                 </div>
+                <div className="profile-options">
+                    <button className="btn primary-btn">
+                        {""}
+                        Hire Me{" "}
+                    </button>
+                    <a href="Arif_khan_(Software Engineer)_03.pdf" download="Arif_khan_(Software Engineer)_03.pdf">
+                        <button className="btn highlighted-btn">Get Resume</button>
+                    </a>
+                </div>
             </div>
-            
+            <div className="profile-picture">
+                    <div className="profile-picture-background"></div>
+            </div>
         </div>
     )
 }
